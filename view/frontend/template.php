b601edd5b96a58996d3d780d70fec5991ebdb8b6
François <francoisbergerot@orange.fr>
2019-11-08 10:37:51 +0100

    Création du fichier template.php (head+header)

---
+++ view/frontend/template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />       
+        <title>Portfolio de François Bergerot</title>
+        <meta description="Portfolio de François Bergerot">       
+        <link  rel="stylesheet" href="./public/css/style.css"/> 
+    </head>
+
+    <header class="display-flex">
+    <div class="logo">
+        
+        <img style="width:80px" src="./public/images/logo.svg" alt="logo"/>
+    </div>
+    
+    <nav class="display-flex">
+        <ul class="display-flex">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Contact</a></li>         
+        </ul>
+    </nav>
+   
+    </header> 
+
+    <body>           
+        <?= $content ?>       
+    </body>
+
+    <footer>    
+    </footer>
+    
+</html>
